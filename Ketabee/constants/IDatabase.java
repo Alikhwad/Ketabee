12575d3b81fc19c4b6e537cc6d3819dc28769777
Ali <alikhwad@gmail.com>
2021-07-30 11:54:36 +0200

    database update

--- Ketabee/constants/IDatabase.java
+++ Ketabee/constants/IDatabase.java
@@ -4,7 +4,7 @@ public class IDatabase {
 
 	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
 	public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/Ketabee?useUnicode=true&characterEncoding=utf-8&serverTimezone=GMT%2B8";
-	public static final String USER_NAME = "root";
+	public static final String USER_NAME = "Ali";
 	public static final String PASSWORD = "AliinTech";
 
 }
